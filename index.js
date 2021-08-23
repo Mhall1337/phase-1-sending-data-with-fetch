5cfe08f8ff51a448ee602c3d737c5d6e0d0ccc0d
Mitchell <Hmitchell.hall@gmail.com>
2021-08-22 21:10:36 -0500

    commit tree 4439953ec607e0167dc719537f7c320cbf3dfa12

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email){
+   return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: { 
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+        .then((response) => response.json())
+         .then((obj) => {console.log(obj)
+            const appendedId = document.createElement('p').textContent = obj.id
+            document.body.append(appendedId)})
+         .catch((error) => {
+             const appendedError = document.createElement('p').textContent = error
+             //appendedError.append(error)
+            document.body.append(appendedError)
+         })
+}
+
+        
+        
+    
+        
+// fetch(`http://localhost:3000/users/${obj.id}`,{
+//     method: 'PATCH',
+//     headers: {
+//         "content-type": "application/json",
+//         "accept": "application/json"
+// },
+// body: JSON.stringify({name, email})
+// })
+// .then(res => res.json())
+// .then(obj => console.log(obj))
